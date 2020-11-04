9b21d511c7a4e4d590af3d7c20722f795948db5b
eg-bond <eg-bond@yandex.ru>
2020-11-04 17:40:27 +0300

    project configuration ended

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
-const path = require('path') //стандартный модуль NodeJS, позволяет удобно работать с путями к файлам
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 const isProd = process.env.NODE_ENV === 'production'
 const isDev = !isProd
@@ -12,32 +12,42 @@ console.log('IS DEV', isDev)
 
 const filename = ext => isDev ? `bundle.${ext}` : `bundle.[hash].${ext}`
 
+const jsloaders = () => {
+    const loaders = [{
+        loader: 'babel-loader',
+        options: {
+            presets: ['@babel/preset-env']
+        },
+    }]
+
+    if (isDev) {
+        loaders.push('eslint-loader')
+    }
+
+    return loaders
+}
+
 module.exports = {
-    context: path.resolve(__dirname, 'src'), //указываем за то, где лежат исходники приложения. __dirname - путь до папки, в которой находится данный файл
-    mode: 'development', //по умолчанию WP работает в режиме production
-    entry: './index.js',
-    output: { //указываем, где будет лежать результат сборки проекта ВебПаком после запуска
-        filename: filename('js'), //название файла. Hash нужен для ...
-        path: path.resolve(__dirname, 'dist') //где находится
+    context: path.resolve(__dirname, 'src'), // указываем за то, где лежат исходники приложения. __dirname - путь до папки, в которой находится данный файл
+    mode: 'development', // по умолчанию WP работает в режиме production
+    entry: ['@babel/polyfill', './index.js'], // полифилл добавляем чтобы не было проблем с regeneratorRuntime
+    output: { // указываем, где будет лежать результат сборки проекта ВебПаком после запуска
+        filename: filename('js'),
+        path: path.resolve(__dirname, 'dist')
     },
     resolve: {
         extensions: ['.js'],
-        alias: { //Присваивает названия путям
+        alias: { // Присваивает названия путям
             '@': path.resolve(__dirname, 'src'),
             '@core': path.resolve(__dirname, 'src/core')
         }
     },
-    devtool: isDev ? 'source-map' : false,
-    // devServer: {
-    //     contentBase: path.join(__dirname, 'dist'),
-    //     compress: true,
-    //     port: 3000
-    // },
+    devtool: isDev ? 'source-map' : false, // Добавляем Source-maps в режиме разработки
     devServer: {
         port: 3000,
         hot: isDev
     },
-    plugins: [ //тут находятся все плагины
+    plugins: [
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
             template: 'index.html',
@@ -60,25 +70,26 @@ module.exports = {
     ],
     module: {
         rules: [
-            { //sass
+            { // sass
                 test: /\.s[ac]ss$/i,
                 use: [
-                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            hmr: isDev,
+                            reloadAll: true
+                        }
+                    },
                     // Translates CSS into CommonJS
                     'css-loader',
                     // Compiles Sass to CSS
                     'sass-loader',
                 ],
             },
-            { //babel
+            { // babel
                 test: /\.m?js$/,
                 exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader",
-                    options: {
-                        presets: ['@babel/preset-env']
-                    }
-                }
+                use: jsloaders()
             }
         ],
     },
