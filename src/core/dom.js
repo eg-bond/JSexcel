bd008d5c3f3b42a8b216eccef76d619bd82fe04f
eg-bond <eg-bond@yandex.ru>
2020-11-08 17:11:19 +0300

    base framework functionality implemented

---
+++ src/core/dom.js
@@ -0,0 +1,57 @@
+class Dom {
+    constructor(selector) {
+        this.$el = typeof selector === 'string'
+            ? document.querySelector(selector) // функция выбирает селектор
+            : selector
+    }
+
+    html(html = '') {
+        if (typeof html === 'string') {
+            this.$el.innerHTML = html
+            return this // мы возвращаем инстанс, чтобы другие методы класса работали там, где это происходит
+        }
+        return this.$el.outerHTML.trim() // trim удаляет пробелы из начала и кончца строки
+    }
+
+    clear() {
+        this.html('')
+        return this
+    }
+
+    on(eventType, callback) {
+        this.$el.addEventListener(eventType, callback)
+    }
+
+    off(eventType, callback) {
+        this.$el.removeEventListener(eventType, callback)
+    }
+
+
+    append(node) { // node - это Element в JavaScript
+        if (node instanceof Dom) {
+            node = node.$el
+        }
+        if (Element.prototype.append) {
+            this.$el.append(node)
+        } else {
+            this.$el.appendChild(node)
+        }
+        return this
+    }
+}
+
+
+export function $(selector) {
+    return new Dom(selector)
+}
+// проделываем такую манипуляцию для того, чтобы не создавать каждый раз инстанс класса через new
+
+$('div').html('<h1>Test</h1>').clear()
+
+$.create = (tagName, classes = '') => {
+    const el = document.createElement(tagName)
+    if (classes) {
+        el.classList.add(classes)
+    }
+    return $(el) // оборачиваем возвращаемый элемент в функцию $, чтобы у нее был весь функционал сласса Dom
+}
